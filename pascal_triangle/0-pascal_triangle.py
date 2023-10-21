26e2f2d4bade497d530ff030eaae27c76dc48b5b
mulimuoki001 <b.muoki@alustudent.com>
2023-10-21 23:35:14 +0200

    pascal's triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -12,6 +12,4 @@ def pascal_triangle(n):
     for i in range(2, n):
         for j in range(1, i):
             triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-    for row in triangle:
-        print(row)
     return triangle
